c4269d30692b61d02708934b8cc89de30e2cb7da
AbdelrahmanElghoul <aelghoul0@gmail.com>
2020-07-26 21:22:07 +0200

    step 1 to 8 done

--- vision_project.py
+++ vision_project.py
@@ -26,44 +26,71 @@ def ReadVideo(path):
     vidcap = cv2.VideoCapture(path)
     success, image = vidcap.read()
     frames = []
+
     while success:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         frames.append(image)
         success, image = vidcap.read()
-
     return frames
 
 
-# step2 Not sure
+# step2
+
 def RGB2HSV(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    (height, width) = image.shape[0:2]
-    out = np.ones(image.shape)
-    for h in range(height):
-        for w in range(width):
-            out[h, w] = rgb_to_hsv(image[h, w][0], image[h, w][1], image[h, w][2])
-    return out
+    #
+    # # for t in image:
+    # #     #print(t)
+    # (height, width) = image.shape[0:2]
+    # out = np.ones(image.shape)
+    # # out0 = [[rgb_to_hsv(y[0], y[1], y[2]) for y in x] for x in image]
+    # for h in range(height):
+    #     for w in range(width):
+    #         out[h, w] = rgb_to_hsv(image[h, w][0], image[h, w][1], image[h, w][2])
+    # tmp=cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # # plt.imshow(cv2.cvtColor(image, cv2.COLOR_RGB2HSV))
+    # # plt.show()
+    #
+    # comparison = out == tmp
+    # equal_arrays = comparison.all()
+    # return out
 
 
-# step2 helper ( not used)
+# step2 helper
 def rgb_to_hsv(r, g, b):
+    # R, G, B values are divided by 255
+    # to change the range from 0..255 to 0..1:
     r, g, b = r / 255.0, g / 255.0, b / 255.0
-    mx = max(r, g, b)
-    mn = min(r, g, b)
-    df = mx - mn
-    if mx == mn:
+
+    # h, s, v = hue, saturation, value
+    cmax = max(r, g, b)  # maximum of r, g, b
+    cmin = min(r, g, b)  # minimum of r, g, b
+    diff = cmax - cmin  # diff of cmax and cmin.
+
+    # if cmax and cmax are equal then h = 0
+    if cmax == cmin:
         h = 0
-    elif mx == r:
-        h = (60 * ((g - b) / df) + 360) % 360
-    elif mx == g:
-        h = (60 * ((b - r) / df) + 120) % 360
-    elif mx == b:
-        h = (60 * ((r - g) / df) + 240) % 360
-    if mx == 0:
+
+    # if cmax equal r then compute h
+    elif cmax == r:
+        h = (60 * ((g - b) / diff) + 360) % 360
+
+    # if cmax equal g then compute h
+    elif cmax == g:
+        h = (60 * ((b - r) / diff) + 120) % 360
+
+    # if cmax equal b then compute h
+    elif cmax == b:
+        h = (60 * ((r - g) / diff) + 240) % 360
+
+    # if cmax equal zero
+    if cmax == 0:
         s = 0
     else:
-        s = (df / mx) * 100
-    v = mx * 100
+        s = (diff / cmax) * 100
+
+    # compute v
+    v = cmax * 100
     return h, s, v
 
 
@@ -74,33 +101,36 @@ def RGB2Gray(image):
 
 # step4  done
 def HSV_Threshold(image):
-
-    aH=image[:,:,0]
-    aS=image[:,:,1]
-    aV=image[:,:,2]
+    # aH = image[:, :, 0]
+    # aS = image[:, :, 1]
+    # aV = image[:, :, 2]
     yr_from = np.array([20, 100, 100])
     yr_to = np.array([30, 255, 255])
     y = HSV_Thresholding(image, yr_from, yr_to)
-    
-    wr_from = np.array([0, 50, 230])
-    wr_to = np.array([255, 50, 255])
+
+    wr_from = np.array([0, 0, 230])
+    wr_to = np.array([255, 30, 255])
     w = HSV_Thresholding(image, wr_from, wr_to)
     # yellow_threshold = cv2.inRange(image, yr_from, yr_to)
     # white_threshold = cv2.inRange(image, wr_from, wr_to)
-    plt.imshow(w)
-    
-    
+
+    # plt.imshow(w)
+    # plt.show()
+    #
+    # plt.imshow(y)
+    # plt.show()
+
     return y, w, np.add(y, w)
 
 
 # step4 helper
-def HSV_Thresholding(image, rangeFrom, rangeTo): 
+def HSV_Thresholding(image, rangeFrom, rangeTo):
     height, width = image.shape[0:2]
-    out = np.zeros(image.shape)
+    out = np.copy(image)
     for h in range(height):
         for w in range(width):
-            if (image[h, w, 0] < rangeTo[0] and image[h, w, 1] < rangeTo[1] and image[h, w, 2] < rangeTo[2]) and \
-                    (image[h, w, 0] > rangeFrom[0] and image[h, w, 1] > rangeFrom[1] and image[h, w, 2] > rangeFrom[
+            if (image[h, w, 0] <= rangeTo[0] and image[h, w, 1] <= rangeTo[1] and image[h, w, 2] <= rangeTo[2]) and \
+                    (image[h, w, 0] >= rangeFrom[0] and image[h, w, 1] >= rangeFrom[1] and image[h, w, 2] >= rangeFrom[
                         2]):
                 out[h, w] = [255, 255, 255]
             else:
@@ -115,35 +145,30 @@ def MaskGrayImage(image, mask):
     # result[mask == 255] = 0
     for h in range(height):
         for w in range(width):
-            if mask[h, w].all() == 255:
-                result[h, w] = 255
+            if mask[h, w].all() == 0:
+                result[h, w] = 0
     return result
 
 
 # step 6 done
-def Gaussian(image, sigma=0.5):
+def Gaussian(image, sigma=0.5, size=3):
     kernel_1 = [[1 / 256, 4 / 256, 6, 256, 4 / 256, 1 / 256],
                 [4 / 256, 16 / 256, 24, 256, 16 / 256, 4 / 256],
                 [6 / 256, 24 / 256, 36, 256, 24 / 256, 6 / 256],
                 [4 / 256, 16 / 256, 24, 256, 16 / 256, 4 / 256],
                 [1 / 256, 4 / 256, 6, 256, 4 / 256, 1 / 256]]
-    kernel = get_gaussian_kernel(sigma)
+    kernel = get_gaussian_kernel(size, sigma)
     out = multiplyKernel(image, kernel)
     return out
 
 
 # step 6 helper calculating Gaussian kernel
-def get_gaussian_kernel(sigma=0.5):
-    if sigma > 1:
-        size = 5
-    else:
-        size = 3
-
+def get_gaussian_kernel(size, sigma=0.5):
     offset = size // 2
     kernel = np.empty([size, size], dtype=float)
     for x in range(size):
         for y in range(size):
-            a = (sigma ** 2) * 2 * np.pi
+            a = (sigma ** 2) * 2 * np.pi  # try sigma * sigma
             nx = x - offset
             ny = y - offset
             b = -1 * (nx ** 2 + ny ** 2)
@@ -161,7 +186,7 @@ def multiplyKernel(image, kernel):
     out = np.copy(image)
     (height, width) = image2.shape[:2]
     for h in range(len(kernel[0] // 2), (height - len(kernel[0] // 2))):
-        #print(h)
+        # #print(h)
         for w in range(len(kernel[1] // 2), (width - len(kernel[1] // 2))):
             tmp = image2[h - (len(kernel[0]) // 2):(1 + h + (len(kernel[0]) // 2)),
                   w - (len(kernel[1]) // 2):(1 + w + (len(kernel[1]) // 2))]
@@ -172,21 +197,20 @@ def multiplyKernel(image, kernel):
 
 
 # step 7
-def CannyEdgeDetection(image,low,high):
+def CannyEdgeDetection(image, low, high):
     sobel, theta = Sobel(image)
     non_maximum_suppression = NonMaximumSuppression(sobel, theta)
     double_thresholding = DoubleThresholding(non_maximum_suppression, high, low)
     edge_detection = HysteresisEdgeTracking(double_thresholding)
 
-    f, axarr = plt.subplots(2, 2)
-    axarr[0, 0].imshow(sobel, cmap='gray')
-    axarr[0, 1].imshow(non_maximum_suppression, cmap='gray')
-    axarr[1, 0].imshow(double_thresholding, cmap='gray')
-    axarr[1, 1].imshow(edge_detection, cmap='gray')
-
-    plt.show()
+    # f, axarr = plt.subplots(2, 2)
+    # axarr[0, 0].imshow(sobel, cmap='gray')
+    # axarr[0, 1].imshow(non_maximum_suppression, cmap='gray')
+    # axarr[1, 0].imshow(double_thresholding)
+    # axarr[1, 1].imshow(edge_detection, cmap='gray')
+    #
+    # plt.show()
     return edge_detection
-
 
 
 # step 7 helper
@@ -195,7 +219,7 @@ def Sobel(image):
                 [-2, 0, 2],
                 [-1, 0, 1]]
     kernel_Y = [[1, 2, 1],
-                [-0, 0, 0],
+                [0, 0, 0],
                 [-1, -2, -1]]
 
     out_X = multiplyKernel(image, kernel_X)
@@ -252,7 +276,7 @@ def DoubleThresholding(image, high_threshold, low_threshold):
             if image[h, w] >= high_threshold:
                 out[h, w] = 255
             elif high_threshold >= image[h, w] >= low_threshold:
-                out[h, w] = 25
+                out[h, w] = 200
             else:
                 out[h, w] = 0
     return out
@@ -261,10 +285,11 @@ def DoubleThresholding(image, high_threshold, low_threshold):
 # step 7 helper
 def HysteresisEdgeTracking(image, size=3):
     height, width = image.shape
+
     out = np.copy(image)
     for h in range(height):
         for w in range(width):
-            if out[h, w] == 25:
+            if out[h, w] == 200:
                 if out[(h - (size // 2)):(h + (size // 2) + 2), (w - (size // 2)):(w + (size // 2) + 2)].any == 255:
                     out[h, w] = 255
                 else:
@@ -272,7 +297,7 @@ def HysteresisEdgeTracking(image, size=3):
     return out
 
 
-# step 8
+# step 9
 def HoughTransofrm(image):
     minLineLength = 150
     # maxLineGap = 5
@@ -293,58 +318,88 @@ def HoughTransofrm(image):
             b = np.sin(theta)
             x0 = a * rho
             y0 = b * rho
-            x1 = int(x0 + 1000 * (-b))
-            y1 = int(y0 + 1000 * (a))
-            x2 = int(x0 - 1000 * (-b))
-            y2 = int(y0 - 1000 * (a))
+            x1 = int(x0 + 1 * (-b))
+            y1 = int(y0 + 1 * (a))
+            x2 = int(x0 - 1 * (-b))
+            y2 = int(y0 - 1 * (a))
 
-            cv2.line(out, (x1, y1), (x2, y2), 255, 8)
-    plt.imshow(out, cmap='gray')
-    plt.show()
+            cv2.line(out, (x1, y1), (x2, y2), 255, 1)
+    # plt.imshow(out, cmap='gray')
+    # plt.show()
 
     return out
+
 
 def main():
     path1 = 'White Lane.mp4'
     path2 = 'Yello Lane.mp4'
     path3 = 'Challenge.mp4'
 
-    frame = ReadVideo(path3)
-    hsvImg = RGB2HSV(frame[0])
-    grayImg = RGB2Gray(frame[0])
-    yellowThreshold, whiteThreshold, thresholdImg = HSV_Threshold(hsvImg)
-    mask1 = MaskGrayImage(grayImg, thresholdImg)
-    
-#    gaussian1 = Gaussian(mask1, 1.5)
-#    gaussian2 = Gaussian(mask1, 1)
-#    gaussian3 = Gaussian(mask1, 0.5)
-    gaussian4 = Gaussian(mask1, 3)
-    
-    canny = CannyEdgeDetection(gaussian4,50,60)
-    cannyMask = MaskGrayImage(gaussian2, canny)
+    from datetime import datetime
+
+    start = datetime.now()
+    frame = ReadVideo(path1)  # step1
+    print("frame", datetime.now() - start)
+
+    start = datetime.now()
+    hsvImg = RGB2HSV(frame[0])  # step2
+    print("RGB2HSV", datetime.now() - start)
+
+    start = datetime.now()
+    grayImg = RGB2Gray(frame[0])  # step3
+    print("RGB2Gray", datetime.now() - start)
+
+    start = datetime.now()
+    yellowThreshold, whiteThreshold, thresholdImg = HSV_Threshold(hsvImg)  # step4
+    print("HSVthreshold", datetime.now() - start)
+
+    start = datetime.now()
+    mask = MaskGrayImage(grayImg, thresholdImg)  # step5
+    print("mask", datetime.now() - start)
+
+    start = datetime.now()
+    # gaussian1 = Gaussian(mask, 1.5)  # step6
+    # gaussian2 = Gaussian(mask1, 1)
+    gaussian3 = Gaussian(mask, 19, 5)
+    # gaussian4 = Gaussian(mask1, 3)
+    print("gaussian", datetime.now() - start)
+
+    gaussian = gaussian3
+
+    start = datetime.now()
+    canny = CannyEdgeDetection(gaussian, 50, 100)  # step7
+    print("canny", datetime.now() - start)
+
+    start = datetime.now()
+    cannyMask = MaskGrayImage(gaussian, canny)  # step8
+    print("cannyMask", datetime.now() - start)
+
     houghTransform = HoughTransofrm(cannyMask)
-    plt.imshow(cannyMask, cmap='gray')
-    plt.imshow(canny, cmap='gray')
-    plt.show()
-    
-    #edges = cv2.Canny(gaussian2,100,200)
-    #plt.imshow(edges, cmap='gray')
-    
-    #plt.imshow(gaussian2,cmap='gray')
-    # f, axarr = plt.subplots(3, 2)
-    #
-    # axarr[0, 0].imshow(frame[0])
-    # axarr[0, 1].imshow(hsvImg)
-    #
-    # axarr[1, 0].imshow(thresholdImg)
-    # axarr[1, 1].imshow(mask)
-    #
-    # axarr[2, 0].imshow(gaussian)
-    # axarr[2, 1].imshow(Gaussian(mask, 1.4))
+
+    # plt.imshow(cannyMask, cmap='gray')
+    # plt.imshow(canny, cmap='gray')
+    # plt.show()
+
+    # edges = cv2.Canny(gaussian2,100,200)
+    # plt.imshow(edges, cmap='gray')
+
+    # plt.imshow(gaussian2,cmap='gray')
+    f, axarr = plt.subplots(3, 2)
+
+    axarr[0, 0].imshow(frame[0])
+    axarr[0, 1].imshow(thresholdImg, cmap='gray')
+
+    axarr[1, 0].imshow(mask, cmap='gray')
+    axarr[1, 1].imshow(gaussian, cmap='gray')
+
+    axarr[2, 0].imshow(canny, cmap='gray')
+    axarr[2, 1].imshow(cannyMask, cmap='gray')
 
     # cv2.imwrite('canny1.jpg', canny)
     # plt.imshow(edges, cmap='gray')
-    # plt.show()
+    plt.show()
+
+    # print("total", datetime.now() - start0)
 
 
 if __name__ == '__main__':
